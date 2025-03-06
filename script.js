1615ee9720c2faf86f1d95eeafda96250833176a
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-03-07 00:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function manipulateArray() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    })
+    .then((arr) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evens = arr.filter(num => num % 2 === 0);
+                document.getElementById('output').innerText = evens.join(', ');
+                resolve(evens);
+            }, 1000);
+        });
+    })
+    .then((evens) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const doubled = evens.map(num => num * 2);
+                document.getElementById('output').innerText = doubled.join(', ');
+                resolve(doubled);
+            }, 2000);
+        });
+    });
+}
+
+manipulateArray();
