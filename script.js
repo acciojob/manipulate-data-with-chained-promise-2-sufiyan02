0036350cc9c2fffe75788be9147331725acf0b1b
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-03-07 00:56:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function manipulateArray() {
-    return new Promise((resolve) => { 
+    return new Promise((resolve) => {
         setTimeout(() => {
-            console.log("Initial array:", [1, 2, 3, 4]); 
+            console.log("Step 1: Initial array [1, 2, 3, 4]");
             resolve([1, 2, 3, 4]);
-        }, 3000);
+        }, 3000); // Initial delay of 3 seconds
     })
     .then((arr) => {
         return new Promise((resolve) => {
             setTimeout(() => {
                 const evens = arr.filter(num => num % 2 === 0);
-                console.log("Filtered evens:", evens);
+                console.log("Step 2: Filtered evens:", evens);
                 document.getElementById('output').innerText = evens.join(', ');
                 resolve(evens);
-            }, 1000);
+            }, 1000); // Delay of 1 second
         });
     })
     .then((evens) => {
         return new Promise((resolve) => {
             setTimeout(() => {
                 const doubled = evens.map(num => num * 2);
-                console.log("Doubled values:", doubled);
+                console.log("Step 3: Doubled values:", doubled);
                 document.getElementById('output').innerText = doubled.join(', ');
                 resolve(doubled);
-            }, 2000);
+            }, 2000); // Delay of 2 seconds
         });
     });
 }
